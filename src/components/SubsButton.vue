afe5e304a162ea896e7df76370044fc3020f86d3
Juanpi94 <juang.94.ucr@gmail.com>
2025-07-11 13:30:05 -0600

    fix: update link in SubsButton component to point to the correct form

--- src/components/SubsButton.vue
+++ src/components/SubsButton.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="pe-3 d-flex justify-content-center align-items-center">
-        <a class="btn btn-primary align-items-center d-flex gap-2" href="#">
+        <a class="btn btn-primary align-items-center d-flex gap-2"
+            href="https://docs.google.com/forms/d/e/1FAIpQLSd1-NvvZcdlqukBblog2wMygB-xj8p7VzM9QADWkeVWbMQeAQ/viewform?usp=header"
+            target="_blank">
             <img class="icon-form" :src="icon_form" alt="">
             Registrarse</a>
     </div>
