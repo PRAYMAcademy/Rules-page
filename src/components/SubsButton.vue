a8aabb6d563e33cfa6c2fd49146517d6b4a4b147
Juanpi94 <juang.94.ucr@gmail.com>
2025-07-23 20:25:59 -0600

    fix: remove font size class from SubsButton component for consistency

--- src/components/SubsButton.vue
+++ src/components/SubsButton.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="pe-3 d-flex justify-content-center align-items-center">
-        <a class="btn btn-primary align-items-center d-flex gap-2 fs-4" :href="link" target="_blank">
+        <a class="btn btn-primary align-items-center d-flex gap-2" :href="link" target="_blank">
             <img class="icon-form" :src="icon_form" alt="">
             {{ texto }}</a>
     </div>
